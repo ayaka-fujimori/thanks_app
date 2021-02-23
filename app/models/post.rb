ab4b1e0a68c6c79e4bfe7709c46255542a7040bd
fujimoon-k <fujimoon.k@gmail.com>
2021-02-23 16:41:57 +0900

    アソシエーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,9 @@ class Post < ApplicationRecord
     validates :user_id, {presence: true}
     
     belongs_to :category
-    belongs_to :user
-    
+    belongs_to :user, class_name: "User"
+    belongs_to :send_user, class_name: "User"
+
     def user
         return User.find_by(id: self.user_id)
     end
